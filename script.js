18843e538026f5eb4325e2f99201e16790290760
José Oliveira dos Santos <jose.victor@vvt.local>
2023-06-15 11:15:54 -0300

    Parte 9

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let cart = [];
+let modalQt =1;
+let modalKey = 0;
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
@@ -14,6 +18,7 @@ pizzaJson.map((item, index) =>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -69,3 +74,15 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
         size.classList.add('selected');
     });
 });
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+    cart.push({
+        id:pizzaJson[modalKey].id, 
+        size,
+        qt:modalQt
+    })
+
+    closeModal();
+})
